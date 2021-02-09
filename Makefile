3eae4e73346017687da865d5ae641cb422807200
Mykola Dorohyi <mdorohyi@c1r7p10.khpi.ucode-connect.study>
2021-02-09 15:46:06 +0200

    Change folder struct

--- Makefile
+++ Makefile
@@ -1,26 +1,35 @@
 .PHONY: uninstall clean reinstall
 
-SERVER_SRCS := $(wildcard build/server/src/*.c)
-CLIENT_SRCS := $(wildcard build/client/src/*.c)
-LIBS_SRCS := $(wildcard build/libs/**/*.c)
+SERVER_SRCS := $(wildcard server/src/*.c)
+CLIENT_SRCS := $(wildcard client/src/*.c)
+UCHAT_SRCS := $(wildcard uchat_gui/src/*.c)
+LIBS_SRCS := $(wildcard libs/**/*.c)
 UNAME := $(shell uname)
 
-server:
+all: clean install  
+
+install: uchat_server test_client uchat
+
+uchat_server:
 	@if [ "$(UNAME)" = "Linux" ]; then \
-	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${SERVER_SRCS} ${LIBS_SRCS} -o server -pthread -ldl -Wno-unknown-pragmas -Wformat; \
+	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${SERVER_SRCS} ${LIBS_SRCS} -o uchat_server -pthread -ldl -Wno-unknown-pragmas -Wformat; \
 	else \
-	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${SERVER_SRCS} ${LIBS_SRCS} -o server -pthread -ldl -Wno-unknown-pragmas; \
+	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${SERVER_SRCS} ${LIBS_SRCS} -o uchat_server -pthread -ldl -Wno-unknown-pragmas; \
 	fi
 
-client:
-	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${CLIENT_SRCS} ${LIBS_SRCS} -o client -pthread -ldl
+test_client:
+	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${CLIENT_SRCS} ${LIBS_SRCS} -o test_client -pthread -ldl
+
+uchat:
+	clang -std=c11 -Wall -Wextra -Werror -Wpedantic ${UCHAT_SRCS} `pkg-config --cflags --libs gtk+-3.0` `pkg-config --cflags --libs json-c` -I uchat_gui/inc/ -o uchat
 
 uninstall: clean
-	@rm -f server
-	@rm -f client
+	@rm -f uchat_server
+	@rm -f test_client
+	@rm -f uchat
 
 clean:
 	@rm -rf server_data
 
-reinstall: uninstall server client
+reinstall: uninstall all
 
--- server/inc/header.h
+++ server/inc/header.h
@@ -2,41 +2,38 @@
 #define JAM_HEADER_H
 #define _POSIX_C_SOURCE 200809L
 
-#include "../../libs/sqlite/sqlite3.h"
-#include "../../libs/list/list.h"
-#include "../../libs/dyad/dyad.h"
-#include "../../libs/frozen/frozen.h"
-
-#include <stdbool.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+#include <arpa/inet.h>
 #include <errno.h>
+#include <netdb.h>
 #include <netinet/in.h>
 #include <pthread.h>
 #include <signal.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/resource.h>  //TODO REmove
 #include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <stdint.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <time.h>
-#include <sys/resource.h> //TODO REmove
+#include <unistd.h>
+
+#include "../../libs/dyad/dyad.h"
+#include "../../libs/list/list.h"
+#include "../../libs/sqlite/sqlite3.h"
 
 #define SERVER_PORT 8000
 #define JSON_OP_SIZE 1024
 extern int errno;
-
 
 typedef struct s_connection {
     int id;
     int uid;
     void (*dataListener)(dyad_Event *);
     dyad_Stream *stream;
-}              t_connection;
-
+} t_connection;
 
 /* Signal handler to handle SIGTERM and SIGINT signals. */
 void signal_handler(int signal_number);
@@ -44,9 +41,9 @@ void signal_handler(int signal_number);
 int generate_unique_id(t_list *connections);
 t_connection *find_node(int id, t_list *connections);
 t_connection *find_node_uid(int uid, t_list *connections);
-bool is_dir_exists(char* name);
+bool is_dir_exists(char *name);
 
-int db_exec(sqlite3 *db, char* querry, char** result);
+int db_exec(sqlite3 *db, char *querry, char **result);
 
 char *mx_itoa(int n);
 int codepoint_len(const uint32_t cp); /* len of associated utf-8 char */
@@ -54,4 +51,4 @@ int utf8_len(const char ch);          /* len of utf-8 encoded char */
 
 char *to_utf8(const uint32_t cp);
 uint32_t to_cp(const char chr[4]);
-#endif //JAM_HEADER_H
+#endif  //JAM_HEADER_H
